a2f1e81de6ab00a46fa58b0dfe63fe5d86e2e5fc
steeezyro <rohanchhaya@gmail.com>
2025-01-20 03:50:52 -0800

    Updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,34 +4,39 @@ import UserGrid from "./components/UserGrid";
 import { useState } from "react";
 
 // updated this after recording. Make sure you do the same so that it can work in production
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 
 function App() {
-	const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-	return (
-		<Stack minH={"100vh"}>
-			<Navbar setUsers={setUsers} />
+  return (
+    <Stack minH={"100vh"}>
+      <Navbar setUsers={setUsers} />
 
-			<Container maxW={"1200px"} my={4}>
-				<Text
-					fontSize={{ base: "3xl", md: "50" }}
-					fontWeight={"bold"}
-					letterSpacing={"2px"}
-					textTransform={"uppercase"}
-					textAlign={"center"}
-					mb={8}
-				>
-					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
-						My Besties
-					</Text>
-					🚀
-				</Text>
+      <Container maxW={"1200px"} my={4}>
+        <Text
+          fontSize={{ base: "3xl", md: "50" }}
+          fontWeight={"bold"}
+          letterSpacing={"2px"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          mb={8}
+        >
+          <Text
+            as={"span"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+          >
+            Social Network
+          </Text>
+          🚀
+        </Text>
 
-				<UserGrid users={users} setUsers={setUsers} />
-			</Container>
-		</Stack>
-	);
+        <UserGrid users={users} setUsers={setUsers} />
+      </Container>
+    </Stack>
+  );
 }
 
 export default App;
